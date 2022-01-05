9b2d65fa21a6c1a379b3919a7e583052bcca340c
Grant <grantgariepy@protonmail.com>
2022-01-04 21:58:44 -0700

    intro mostly done, about started

---
+++ react-portfolio/src/components/About/About.jsx
@@ -0,0 +1,15 @@
+import "./about.css"
+
+export const About = () => {
+    return (
+        <div className="a">
+            <div className="a-left">
+              <div className="a-card bg"></div>  
+              <div className="a-card">
+                  <img src="https://images.pexels.com/photos/3585047/pexels-photo-3585047.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="a-img" />
+                  </div>  
+            </div>
+            <div className="a-right"></div>
+        </div>
+    )
+}
