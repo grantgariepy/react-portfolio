1aca6f66847c9d6df79bb44ee2ffa28a271de2db
Grant <grantgariepy@protonmail.com>
2022-01-05 20:48:01 -0700

    finished about me section, made layout for project section

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,5 +1,6 @@
 import { About } from "./components/About/About";
 import Intro from "./components/intro/Intro";
+import ProductList from "./components/productList/ProductList";
 
 
 
@@ -7,6 +8,7 @@ const App = () => {
   return <div>
     <Intro/>
     <About/>
+    <ProductList/>
     </div>;
 };
 
---
+++ react-portfolio/src/data.js
@@ -0,0 +1,36 @@
+
+
+export const products = [
+    {
+      id: 1,
+      img: "http://pngimg.com/uploads/bitcoin/bitcoin_PNG25.png",
+      link: "http://lama.dev",
+    },
+    {
+      id: 2,
+      img: "https://clipground.com/images/imdb-icon-clipart-1.jpg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 3,
+      img: "https://www.nielsenanimal.com/wp-content/uploads/2018/09/mens-nike-athletic-dry-t-shirt-blue-t-shirts_1.jpg",
+      link: "http://lama.dev",
+    },
+    {
+      id: 4,
+      img: "",
+      link: "http://lama.dev",
+    },
+    {
+      id: 5,
+      img: "",
+      link: "http://lama.dev",
+    },
+    {
+      id: 6,
+      img: "",
+      link: "http://lama.dev",
+    },
+  ];
+
+//   NEED TO INSTERT PICS OF MY PROJECTS
