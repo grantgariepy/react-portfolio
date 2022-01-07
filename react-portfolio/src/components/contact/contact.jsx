157e5e7c35e7a1838e2e8eb03599ce3936a0a1da
Grant <grantgariepy@protonmail.com>
2022-01-06 21:17:41 -0700

    contact form done, and emailjs set up and working

--- react-portfolio/src/components/contact/contact.jsx
+++ react-portfolio/src/components/contact/contact.jsx
@@ -2,8 +2,34 @@ import "./contact.css"
 import Phone from "../../img/phone.png"
 import Email from "../../img/email.png"
 import Address from "../../img/address.png"
+import emailjs from 'emailjs-com';
+import {  useRef, useState } from "react"
 
 const Contact = () => {
+
+        const formRef = useRef();
+        const [done, setDone] = useState(false)
+
+        const handleSubmit = (e) => {
+            e.preventDefault();
+            emailjs
+              .sendForm(
+                "service_h0nmafe",
+                "template_oh7f94r",
+                formRef.current,
+                "user_qahuVYXXIY9wYiGASJ0CB"
+              )
+              .then(
+                (result) => {
+                  console.log(result.text);
+                  setDone(true)
+                },
+                (error) => {
+                  console.log(error.text);
+                }
+              );
+          };   
+
     return (
         <div className="c">
             <div className="c-bg"></div>
@@ -20,7 +46,7 @@ const Contact = () => {
                         </div>
                         <div className="c-info-item">
                             <img src={Email} alt="" className="c-icon" />
-                            grantgariepy@protonmail.com
+                            grant.gariepy@gmail.com
                         </div>
                         <div className="c-info-item">
                             <img src={Address} alt="" className="c-icon" />
@@ -33,12 +59,13 @@ const Contact = () => {
                         <b>What's your story?</b> Get in touch. Always 
                         freelancing if the right project comes along.
                     </p>
-                    <form>
+                    <form ref={formRef} onSubmit={handleSubmit}>
                         <input type="text" placeholder="Name" name="user_name" />
                         <input type="text" placeholder="Subject" name="user_subject" />
                         <input type="text" placeholder="Email" name="user_email" />
-                        <textarea rows="5" placeholder="Message" name="message"></textarea>
+                        <textarea rows="5" placeholder="Message" name="message" />
                         <button>Submit</button>
+                        {done && "Thank you..."}
                     </form>
                 </div>
             </div>
