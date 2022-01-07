846c75981a1e5cee2e9a3528458bdfdee73a3969
Grant <grantgariepy@protonmail.com>
2022-01-06 20:37:59 -0700

    submit form

--- react-portfolio/src/components/contact/contact.jsx
+++ react-portfolio/src/components/contact/contact.jsx
@@ -37,6 +37,8 @@ const Contact = () => {
                         <input type="text" placeholder="Name" name="user_name" />
                         <input type="text" placeholder="Subject" name="user_subject" />
                         <input type="text" placeholder="Email" name="user_email" />
+                        <textarea rows="5" placeholder="Message" name="message"></textarea>
+                        <button>Submit</button>
                     </form>
                 </div>
             </div>
