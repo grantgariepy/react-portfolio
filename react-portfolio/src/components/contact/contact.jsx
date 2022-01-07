233a17d31741974a41b64a1af05b7102ce6e3f6a
Grant <grantgariepy@protonmail.com>
2022-01-06 20:27:37 -0700

    beginning of contact section

--- react-portfolio/src/components/contact/contact.jsx
+++ react-portfolio/src/components/contact/contact.jsx
@@ -1,7 +1,35 @@
+import "./contact.css"
+import Phone from "../../img/phone.png"
+import Email from "../../img/email.png"
+import Address from "../../img/address.png"
+
 const Contact = () => {
     return (
-        <div>
-            contact
+        <div className="c">
+            <div className="c-bg"></div>
+            <div className="c-wrapper">
+                <div className="c-left">
+                    <h1 className="c-title">Let's discuss your project</h1>
+                    <div className="c-info">
+                        <div className="c-info-item">
+                            <img 
+                            src={Phone}
+                            alt="" 
+                            className="c-icon" />
+                            (801)726-0955
+                        </div>
+                        <div className="c-info-item">
+                            <img src={Email} alt="" className="c-icon" />
+                            grantgariepy@protonmail.com
+                        </div>
+                        <div className="c-info-item">
+                            <img src={Address} alt="" className="c-icon" />
+                            Salt Lake City, Utah
+                        </div>
+                    </div>
+                </div>
+                <div className="c-right"></div>
+            </div>
         </div>
     )
 }
